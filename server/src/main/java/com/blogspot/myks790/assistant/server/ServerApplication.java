25e57d2b7ee992e374527bc5c2ad46be60c4420f
myks790 <myks790@gmail.com>
2018-06-02 15:27:49 +0900

    스프링 부트, DB 기본 설정

---
+++ server/src/main/java/com/blogspot/myks790/assistant/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.blogspot.myks790.assistant.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
