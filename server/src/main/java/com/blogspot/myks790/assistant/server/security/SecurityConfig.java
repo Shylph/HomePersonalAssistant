4e92ee1e51cbaf56eab0b192e5f4bcbc0a498cc1
myks790 <myks790@gmail.com>
2018-06-11 01:32:32 +0900

    test용 SecurityConfigForTest 삭제 SecurityConfig에서 프로파일 확인하도록 변경

--- server/src/main/java/com/blogspot/myks790/assistant/server/security/SecurityConfig.java
+++ server/src/main/java/com/blogspot/myks790/assistant/server/security/SecurityConfig.java
@@ -2,8 +2,9 @@ package com.blogspot.myks790.assistant.server.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -11,13 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.Arrays;
+
 @Slf4j
 @EnableWebSecurity
-@Profile("production")
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Autowired
+    private final Environment environment;
     private final AuthorizationService authorizationService;
     private final AuthProvider authProvider;
 
@@ -30,34 +33,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
         log.info("SecurityConfig configure web");
-        web.ignoring().antMatchers("/js/**");
+        web.ignoring().antMatchers("/js/**","/css/**","/images/**");
     }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         log.info("SecurityConfig configure http");
-        http.authorizeRequests()
-                .antMatchers("/", "/login", "/login-error", "/login-processing").permitAll()
-                .antMatchers("/**").authenticated();
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            log.info("SecurityConfig configure http - test profiles setting");
+            http.csrf().disable();
+            http.authorizeRequests().antMatchers("/**").permitAll();
+        } else {
+            http.authorizeRequests()
+                    .antMatchers("/", "/login", "/login-error", "/login-processing").permitAll()
+                    .antMatchers("/**").authenticated();
 
-        http.formLogin()
-                .loginPage("/")
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .failureUrl("/login-error")
-                .defaultSuccessUrl("/home", true)
-                .usernameParameter("id")
-                .passwordParameter("password");
+            http.formLogin()
+                    .loginPage("/")
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
+                    .failureUrl("/login-error")
+                    .defaultSuccessUrl("/home", true)
+                    .usernameParameter("id")
+                    .passwordParameter("password");
 
-        http.logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true);
+            http.logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true);
 
-        http.authenticationProvider(authProvider);/*
-                http.csrf().disable();
-                http.authorizeRequests().antMatchers("/**").permitAll();*/
+            http.authenticationProvider(authProvider);
+        }
+
     }
-
 }
